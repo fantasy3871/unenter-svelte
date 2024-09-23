4e626aa710ef13fb8c00c87eb180777998003cea
fantasy3871 <89033750+fantasy3871@users.noreply.github.com>
2024-09-23 21:54:41 +1000

    feat: add NoiseMap page

---
+++ src/routes/NoiseMap.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+  import { onMount } from 'svelte';
+  let canvasRef: HTMLCanvasElement;
+
+  onMount(() => {
+
+  })
+</script>
+
+<div class="flex-grow m-3 overflow-hidden" style="width: 100%; height: 100%;">
+  <canvas bind:this={canvasRef} width="500" height="300" style="width: 100%; height: 100%;"></canvas>
+</div>
+
+<style>
+  .flex-grow {
+    flex-grow: 1;
+  }
+  .m-3 {
+    margin: 1rem;
+  }
+  .overflow-hidden {
+    overflow: hidden;
+  }
+</style>
