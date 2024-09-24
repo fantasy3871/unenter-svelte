a1c3bf8ea089f11eb0ccddfd3ed7ccae2296c82c
fantasy3871 <89033750+fantasy3871@users.noreply.github.com>
2024-09-24 23:08:51 +1000

    fix: name of input

--- src/routes/NoiseMapControl.svelte
+++ src/routes/NoiseMapControl.svelte
@@ -5,12 +5,12 @@
 <div>
 	<div class="flex flex-row flex-wrap gap-3 md:gap-10 px-5 md:px-10 py-5">
 		<div class="flex flex-col mb-4">
-			<label class="block text-gray-700 text-sm font-bold mb-2" for="username">
+			<label class="block text-gray-700 text-sm font-bold mb-2" for="terrainSeed">
 				Terrain Seed
 			</label>
 			<input
 				class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-				id="username"
+				id="terrainSeed"
 				type="text"
 			/>
 		</div>
@@ -52,7 +52,7 @@
 			<label class="block text-gray-700 text-sm font-bold mb-2" for="octaves"> octaves </label>
 			<input
 				class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
-				id="password"
+				id="octaves"
 				type="text"
 			/>
 		</div>
