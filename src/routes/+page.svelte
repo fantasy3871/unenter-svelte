5476de848a387be0416ac51c23a6ef554ce80d89
fantasy3871 <89033750+fantasy3871@users.noreply.github.com>
2024-09-24 22:33:37 +1000

    refactor: run: format prettier

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,14 @@
 <script lang="ts">
-  import NoiseMap from "./NoiseMap.svelte";
+	import NoiseMap from './NoiseMap.svelte';
+	import NoiseMapControl from './NoiseMapControl.svelte';
 </script>
 
 <div class="flex flex-col h-screen min-w-96">
-  <NoiseMap />
+	<NoiseMap />
+	<div class="flex flex-row flex-wrap gap-3 md:gap-10 px-5 md:px-10 py-5">
+		<NoiseMapControl />
+	</div>
 </div>
 
 <style>
-  
 </style>
--- src/routes/NoiseMap.svelte
+++ src/routes/NoiseMap.svelte
@@ -1,21 +1,22 @@
 <script lang="ts">
-  import { onMount } from 'svelte';
-  let canvasRef: HTMLCanvasElement;
+	import { onMount } from 'svelte';
+	let canvasRef: HTMLCanvasElement;
 
-  onMount(() => {
-    if (canvasRef) {
-      const ctx = canvasRef.getContext('2d');
-      if (ctx) {
-        ctx.fillStyle = 'lightblue';
-        console.log("canvasRef.width, canvasRef.height: ", canvasRef.width, ", ", canvasRef.height)
-        ctx.fillRect(0, 0, canvasRef.width, canvasRef.height);
-      }
-    }
-  });
+	onMount(() => {
+		if (canvasRef) {
+			const ctx = canvasRef.getContext('2d');
+			if (ctx) {
+				ctx.fillStyle = 'lightblue';
+				console.log('canvasRef.width, canvasRef.height: ', canvasRef.width, ', ', canvasRef.height);
+				ctx.fillRect(0, 0, canvasRef.width, canvasRef.height);
+			}
+		}
+	});
 </script>
 
-<div class="flex-grow m-3 overflow-hidden" style="width: 100%; height: 100%;">
-  <canvas bind:this={canvasRef} width="500" height="300" style="width: 100%; height: 100%;"></canvas>
+<div class="flex-grow p-3 overflow-hidden" style="width: 100%; height: 100%;">
+	<canvas bind:this={canvasRef} width="500" height="300" style="width: 100%; height: 100%;"
+	></canvas>
 </div>
 
 <style>
