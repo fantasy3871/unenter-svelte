1baf5b12e4a7281322c192d8e25ad35d14e13ff0
fantasy3871 <89033750+fantasy3871@users.noreply.github.com>
2024-09-26 22:46:17 +1000

    feat: add noise map

--- src/routes/NoiseMap.svelte
+++ src/routes/NoiseMap.svelte
@@ -1,23 +1,152 @@
 <script lang="ts">
-	import { onMount } from 'svelte';
-	let canvasRef: HTMLCanvasElement;
+	import { onMount, onDestroy } from 'svelte';
+	import { defineHex, Grid, Hex, Orientation, rectangle } from 'honeycomb-grid';
+	import { Application, Container, Graphics } from 'pixi.js';
+	import type { TerrainLayer } from '../types/noise-map';
+	import { TILE } from '../constants/noise-map';
+	import {
+		applyShadeAndFindMidpoint,
+		createNoiseFunction,
+		drawHex,
+		getColor,
+		getNoiseShade,
+		getShade
+	} from '../utils/noise-map';
+	import { noiseMapData } from '../stores/noise-map';
 
-	onMount(() => {
+	let terrainSeed: string;
+	let tileSize: number;
+	let frequency: number;
+	let lacunarity: number;
+	let octaves: number;
+	let persistence: number;
+	let orientation: string;
+	let scale: number;
+	let tileStyle: string;
+	let terrainLayers: TerrainLayer[];
+	let canvasRef: HTMLCanvasElement;
+	let app: Application;
+	let gridRef: Grid<Hex>;
+	const graphics = new Graphics();
+	const stageContainer = new Container();
+	let Tile: ReturnType<typeof defineHex>;
+
+	noiseMapData.subscribe((value) => {
+		terrainSeed = value.worldData.terrainSeed;
+		tileSize = value.tileSize;
+		frequency = value.frequency;
+		lacunarity = value.lacunarity;
+		octaves = value.octaves;
+		persistence = value.persistence;
+		orientation = value.worldData.orientation;
+		scale = value.scale;
+		tileStyle = value.tileStyle;
+		terrainLayers = value.terrainLayers;
+	});
+
+	const resizeCanvas = () => {
 		if (canvasRef) {
-			const ctx = canvasRef.getContext('2d');
-			if (ctx) {
-				ctx.fillStyle = 'lightblue';
-				console.log('canvasRef.width, canvasRef.height: ', canvasRef.width, ', ', canvasRef.height);
-				ctx.fillRect(0, 0, canvasRef.width, canvasRef.height);
+			const parent = canvasRef.parentElement;
+			if (parent) {
+				canvasRef.width = parent.clientWidth;
+				canvasRef.height = parent.clientHeight;
 			}
 		}
+	};
+	onMount(() => {
+		window.addEventListener('resize', resizeCanvas);
+
+		onDestroy(() => {
+			window.removeEventListener('resize', resizeCanvas);
+			app?.destroy(true); // Clean up Pixi.js when component is destroyed
+		});
 	});
+
+	// Reactive to update map generation on change of certain variables
+	$: {
+		if (canvasRef) {
+			resizeCanvas();
+			Tile = defineHex({
+				dimensions: tileSize,
+				origin: 'topLeft',
+				orientation: Orientation.POINTY
+			});
+
+			app = new Application();
+			app.init({
+				canvas: canvasRef, // Attach the canvas
+				antialias: true,
+				backgroundColor: 0x242120,
+				resizeTo: canvasRef?.parentElement ?? undefined
+			});
+
+			const mapNoise2D = createNoiseFunction(terrainSeed, {
+				amplitude: 1,
+				frequency,
+				lacunarity,
+				octaves,
+				persistence
+			});
+			const generateMap = () => {
+				graphics.clear();
+				const tile = new Tile();
+
+				let width, height;
+				if (orientation === Orientation.POINTY) {
+					width = Math.floor(canvasRef.clientWidth / tile.width);
+					height = Math.floor(canvasRef.clientHeight / ((3 / 4) * tile.height));
+				} else {
+					width = Math.floor(canvasRef.clientWidth / ((3 / 4) * tile.width));
+					height = Math.floor(canvasRef.clientHeight / tile.height);
+				}
+
+				gridRef = new Grid(Tile, rectangle({ width, height }));
+
+				gridRef.forEach((hex) => {
+					const elevation = mapNoise2D(hex.x / scale, hex.y / scale);
+					let fillColor;
+
+					switch (tileStyle) {
+						case TILE.STYLES.FLAT:
+							fillColor = getColor(elevation, terrainLayers);
+							break;
+						case TILE.STYLES.NOISE:
+							fillColor = getNoiseShade(elevation);
+							break;
+						case TILE.STYLES.TERRAIN:
+							fillColor = applyShadeAndFindMidpoint(
+								getColor(elevation, terrainLayers),
+								getShade(elevation)
+							);
+							break;
+						default:
+							fillColor = '#474544';
+					}
+
+					drawHex(hex, graphics, { fillColor });
+				});
+			};
+			generateMap();
+			stageContainer.addChild(graphics);
+			app.stage.addChild(stageContainer);
+		}
+	}
 </script>
 
-<div class="flex-grow p-3 overflow-hidden" style="width: 100%; height: 100%;">
-	<canvas bind:this={canvasRef} width="500" height="300" style="width: 100%; height: 100%;"
-	></canvas>
+<!-- Svelte Template -->
+<div class="map-container">
+	<canvas bind:this={canvasRef}></canvas>
 </div>
 
 <style>
+	.map-container {
+		flex-grow: 1;
+		margin: 1rem;
+		overflow: hidden;
+	}
+
+	canvas {
+		width: 100%;
+		height: 100%;
+	}
 </style>
--- src/routes/NoiseMapControl.svelte
+++ src/routes/NoiseMapControl.svelte
@@ -1,5 +1,49 @@
-<script>
-	let name = 'world';
+<script lang="ts">
+	import { noiseMapData } from '../stores/noise-map';
+	import { TILE } from '../constants/noise-map';
+	import { get } from 'svelte/store';
+
+	let terrainSeed: string;
+	let scale: number;
+	let tileStyle: string;
+	let frequency: number;
+	let lacunarity: number;
+	let octaves: number;
+	let persistence: number;
+
+	noiseMapData.subscribe((value) => {
+		terrainSeed = value.worldData.terrainSeed;
+		scale = value.scale;
+		tileStyle = value.tileStyle;
+		frequency = value.frequency;
+		lacunarity = value.lacunarity;
+		octaves = value.octaves;
+		persistence = value.persistence;
+	});
+
+	function updateTerrainSeed(event: Event) {
+		const newSeed = (event.target as HTMLInputElement).value;
+		noiseMapData.update((data) => {
+			return {
+				...data,
+				worldData: {
+					...data.worldData,
+					terrainSeed: newSeed
+				}
+			};
+		});
+	}
+
+	function updateNoiseMapData(event: Event) {
+		const field = (event.target as HTMLInputElement).id;
+		const newValue = (event.target as HTMLInputElement).value;
+		noiseMapData.update((data) => {
+			return {
+				...data,
+				[field]: newValue
+			};
+		});
+	}
 </script>
 
 <div>
@@ -9,6 +53,8 @@
 				Terrain Seed
 			</label>
 			<input
+				bind:value={terrainSeed}
+				on:change={updateTerrainSeed}
 				class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
 				id="terrainSeed"
 				type="text"
@@ -17,6 +63,8 @@
 		<div class="flex flex-col mb-4">
 			<label class="block text-gray-700 text-sm font-bold mb-2" for="scale"> Scale </label>
 			<input
+				bind:value={scale}
+				on:change={updateNoiseMapData}
 				class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
 				id="scale"
 				type="text"
@@ -24,15 +72,22 @@
 		</div>
 		<div class="flex flex-col mb-4">
 			<label class="block text-gray-700 text-sm font-bold mb-2" for="style"> Style </label>
-			<input
+			<select
+				bind:value={tileStyle}
+				on:change={updateNoiseMapData}
 				class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
-				id="style"
-				type="text"
-			/>
+				id="tileStyle"
+			>
+				{#each Object.values(TILE.STYLES) as style}
+					<option value={style}>{style}</option> <!-- Display the style value -->
+				{/each}
+			</select>
 		</div>
 		<div class="flex flex-col mb-4">
 			<label class="block text-gray-700 text-sm font-bold mb-2" for="frequency"> Frequency </label>
 			<input
+				bind:value={frequency}
+				on:change={updateNoiseMapData}
 				class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
 				id="frequency"
 				type="text"
@@ -43,6 +98,8 @@
 				Lacunarity
 			</label>
 			<input
+				bind:value={lacunarity}
+				on:change={updateNoiseMapData}
 				class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
 				id="lacunarity"
 				type="text"
@@ -51,6 +108,8 @@
 		<div class="flex flex-col mb-4">
 			<label class="block text-gray-700 text-sm font-bold mb-2" for="octaves"> octaves </label>
 			<input
+				bind:value={octaves}
+				on:change={updateNoiseMapData}
 				class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
 				id="octaves"
 				type="text"
@@ -61,6 +120,8 @@
 				Persistence
 			</label>
 			<input
+				bind:value={persistence}
+				on:change={updateNoiseMapData}
 				class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
 				id="persistence"
 				type="text"
